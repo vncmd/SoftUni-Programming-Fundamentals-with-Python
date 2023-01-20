79b789924955e3b278bdf9cc93e19f83e2891d51
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-20 15:11:43 +0100

    Deleted Files

--- Programming Fundamentals with Python SoftUni/Week 1 Basic Syntax, Conditional Statements and Loops  - Exercise/string_pureness.py
+++
@@ -1,8 +0,0 @@
-number_strs = int(input())
-
-for string in range(number_strs):
-    current_string = input()
-    if "," in current_string or "." in current_string or "_" in current_string:
-        print(f"{current_string} is not pure!")
-    else:
-        print(f"{current_string} is pure.")
