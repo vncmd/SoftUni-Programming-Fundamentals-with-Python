16d5d087763db0161bba53103ff53c32ca4c62ed
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-22 13:07:33 +0100

    Rock, Paper, Scissors assigned project from Fundamentals course.

---
+++ [0]Projects/rock_paper_scissors.py
@@ -0,0 +1,12 @@
+rock = "Rock"
+paper = "paper"
+scissors = "Scissors"
+
+player_move = input("Choose [r]ock, [p]paper or [s]cissors: ")
+
+if player_move == "r":
+    player_move = rock
+elif player_move == "p":
+    player_move = paper
+elif player_move == "s":
+    player_move = scissors
