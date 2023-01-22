23c3299555085a51894808a7356bbcdf8b31549d
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-22 13:17:10 +0100

    Update rock_paper_scissors.py

--- [0]Projects/rock_paper_scissors.py
+++ [0]Projects/rock_paper_scissors.py
@@ -1,3 +1,4 @@
+import random
 rock = "Rock"
 paper = "paper"
 scissors = "Scissors"
@@ -12,3 +13,15 @@ elif player_move == "s":
     player_move = scissors
 else:
     raise SystemExit("Invalid Input. Try again...")
+
+computer_random_num = random.randint(1, 3)
+
+computer_move = ""
+if computer_random_num == 1:
+    computer_move = rock
+elif computer_move == 2:
+    computer_move = paper
+else:
+    computer_move = scissors
+    
+
