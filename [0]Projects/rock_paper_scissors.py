a5dd4b9fef62efb7060e1f3307eb864ec12309ef
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-22 13:25:40 +0100

    Update rock_paper_scissors.py

--- [0]Projects/rock_paper_scissors.py
+++ [0]Projects/rock_paper_scissors.py
@@ -3,7 +3,7 @@ rock = "Rock"
 paper = "paper"
 scissors = "Scissors"
 
-player_move = input("Choose [r]ock, [p]paper or [s]cissors: ")
+player_move = input("Choose rock, paper or scissors: ")
 
 if player_move == "r":
     player_move = rock
@@ -23,5 +23,16 @@ elif computer_move == 2:
     computer_move = paper
 else:
     computer_move = scissors
-    
+
+if (player_move == rock and computer_move == scissors) or \
+        (player_move == paper and computer_move == rock) or \
+        (player_move == scissors and computer_move == paper):
+    print("You win!")
+elif player_move == computer_move:
+    print("Draw!")
+else:
+    print("You lose!")
+
+
+
 
