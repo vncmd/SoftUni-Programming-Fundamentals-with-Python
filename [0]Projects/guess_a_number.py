2d167ecd6ac75b4d7ddd69a48cb527ac84e196d3
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-23 11:18:40 +0100

    Basic project from SoftUni Fundamentals with Python course.

---
+++ [0]Projects/guess_a_number.py
@@ -0,0 +1,26 @@
+from random import randint
+from colorama import Fore
+
+computer_num = randint(1, 100)
+
+while True:
+    player_input = input("Guess the number (1-100): ")
+
+    if not player_input.isdigit():
+        print(f"{Fore.RED}Well that's an invalid input. You can try again.")
+        continue
+
+    player_number = int(player_input)
+
+    if player_number == computer_num:
+        print(f"{Fore.GREEN}Well, you guessed it!")
+        break
+
+    elif player_number > computer_num:
+        print(f"{Fore.LIGHTYELLOW_EX}That's... Too High!")
+        break
+
+    else:
+        print(f"{Fore.LIGHTYELLOW_EX}Nope... that's Too Low!")
+        break
+
