8d181b6db07ed85462f407338c2b2bc3f25e1c23
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-27 13:24:47 +0100

    Create numbers_filter.py
    Output filtered lists.

---
+++ [3] Lists Basics/Lists Basics - Lab/numbers_filter.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+positive = []
+negative = []
+even = []
+odd = []
+
+for nums in range(n):
+    current_num = int(input())
+
+    if current_num >= 0:
+        positive.append(current_num)
+    else:
+        negative.append(current_num)
+
+    if current_num % 2 == 0:
+        even.append(current_num)
+    else:
+        odd.append(current_num)
+
+command = input()
+
+if command == "even":
+    print(even)
+elif command == "odd":
+    print(odd)
+elif command == "positive":
+    print(positive)
+else:
+    print(negative)
