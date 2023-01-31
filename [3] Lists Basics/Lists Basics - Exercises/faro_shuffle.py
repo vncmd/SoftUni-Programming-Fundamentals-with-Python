62218eb44321cf7025c1f865ab917c064a36cdb4
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-01-31 13:24:58 +0100

    Update faro shuffle.

--- [3] Lists Basics/Lists Basics - Exercises/faro_shuffle.py
+++ [3] Lists Basics/Lists Basics - Exercises/faro_shuffle.py
@@ -1,12 +1,27 @@
 cards = input().split()
 shuffles = int(input())
 
-first_half = []
-second_half = []
-for idx in range(shuffles):
-    card = cards [idx]
-    if idx < len(cards) / 2:
-        first_half.append(card)
-    else:
-        second_half.append(card)
+for i in range(shuffles):
+    first_half = []
+    second_half = []
 
+    for idx in range(1, len(cards) - 1):
+        card = cards[idx]
+        if idx < len(cards) / 2:
+            first_half.append(card)
+        else:
+            second_half.append(card)
+
+    shuffled = []
+    first_part_idx = 0
+    second_part_idx = 0
+    for idx in range(len(first_half) * 2):
+        if idx % 2 == 0:
+            shuffled.append(second_half[second_part_idx])
+            second_part_idx += 1
+        else:
+            shuffled.append(first_half[first_part_idx])
+            first_part_idx += 1
+
+    cards = [cards[0]] + shuffled + [cards[-1]]
+print(cards)
