506e3e3348b434f4d120b87ebbd42989a4d6e712
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-02 16:07:11 +0100

    Output function calculates returns area of rectangle from input.

---
+++ [4] Functions/Functions - Lab/6_calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def calc_area_rectangle(width, height):
+    result = width * height
+    return result
+
+
+width_rectangle = int(input())
+height_rectangle = int(input())
+print(calc_area_rectangle(width_rectangle, height_rectangle))
