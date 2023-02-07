acf7649d6fe24d7e8ff7ab607a8e22843dd6a57f
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-07 19:38:15 +0100

    Update loading bar different way of the solution.

--- [4] Functions/[1] Functions - Exercises/10_loading_bar.py
+++ [4] Functions/[1] Functions - Exercises/10_loading_bar.py
@@ -1,11 +1,8 @@
-def loading_bar(nums):
-    num_range = int(nums / 10)
-    target = 10
-    if target == num_range:
-        return "100% Complete!\n" + "[" + target * "%" + "]"
-    else:
-        return f"{nums}% " + "[" + num_range * "%" + (target - num_range) * "." + "]\n" + "Still loading..."
+def status(number):
+    if number == 100:
+        return "100% Complete!\n[%%%%%%%%%%]"
+    return f"{number}% [{'%' * (number // 10)}{'.' * (10 - number // 10)}]\nStill loading..."
 
 
-number = int(input())
-print(loading_bar(number))
+single_number = int(input())
+print(status(single_number))
