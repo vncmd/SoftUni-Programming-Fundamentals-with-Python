e21cc2fc480fdb41d83291f541c4d700c19ee0ff
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-14 19:40:42 +0100

    Update anonymous threat.

--- [5] Lists Advanced/[1] Lists Advanced - Exercises/08_anonymous_threat.py
+++ [5] Lists Advanced/[1] Lists Advanced - Exercises/08_anonymous_threat.py
@@ -2,35 +2,35 @@ def merge(start, end):
     if start < 0:
         start = 0
     if start < end:
-        length = len(main_str)
+        length = len(words)
         if end >= length:
             end = length - 1
         for num in range(start, end):
-            main_str[start] += f"{main_str.pop(start + 1)}"
+            words[start] += f"{words.pop(start + 1)}"
 
 
 def divide(idx, partitions):
-    length = len(main_str[idx])
+    length = len(words[idx])
     space_between = length // partitions
-    string_to_change = main_str.pop(idx)
+    string_to_change = words.pop(idx)
     result_ = []
     for x in range(partitions - 1):
         result_.append(string_to_change[:space_between])
         string_to_change = string_to_change[space_between:]
     result_.append(string_to_change)
     for x in result_[::-1]:
-        main_str.insert(idx, x)
+        words.insert(idx, x)
 
 
-main_str = input().split()
-commands = input()
+words = input().split()
+line = input()
 
-while commands != "3:1":
-    command, start_index, end_index = [int(x) if x[-1].isdigit() else x for x in commands.split()]
+while line != "3:1":
+    command, start_index, end_index = [int(x) if x[-1].isdigit() else x for x in line.split()]
     if command == "merge":
         merge(start_index, end_index)
     elif command == "divide":
         divide(start_index, end_index)
-    commands = input()
+    line = input()
 
-print(" ".join(main_str))
+print(" ".join(words))
