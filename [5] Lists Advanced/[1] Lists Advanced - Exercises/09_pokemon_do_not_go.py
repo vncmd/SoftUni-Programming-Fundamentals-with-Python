eb4aa1665280a3b9dbdac7e298012bdc1c1eddb3
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-14 20:16:40 +0100

    Update.

--- [5] Lists Advanced/[1] Lists Advanced - Exercises/09_pokemon_do_not_go.py
+++ [5] Lists Advanced/[1] Lists Advanced - Exercises/09_pokemon_do_not_go.py
@@ -3,16 +3,16 @@ distance = [int(x) for x in input().split()]
 result = []
 
 while distance:
-    index_ = int(input())
+    index = int(input())
     caught_pokemon = ""
-    if index_ < 0:
+    if index < 0:
         caught_pokemon = distance.pop(0)
         distance.insert(0, distance[-1])
-    elif index_ >= len(distance):
+    elif index >= len(distance):
         caught_pokemon = distance.pop(-1)
         distance.append(distance[0])
     if not caught_pokemon:
-        caught_pokemon = distance.pop(index_)
+        caught_pokemon = distance.pop(index)
     result.append(caught_pokemon)
     for idx, pokemon in enumerate(distance):
         if pokemon <= caught_pokemon:
