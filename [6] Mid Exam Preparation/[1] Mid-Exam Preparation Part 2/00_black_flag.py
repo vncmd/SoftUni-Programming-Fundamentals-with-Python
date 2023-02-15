ee0adefed7a9c7b6f829d957c13deaad6f41868c
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-15 21:24:05 +0100

    Output program checks if target plunder is reached based on inputted values.

---
+++ [6] Mid Exam Preparation/[1] Mid-Exam Preparation Part 2/00_black_flag.py
@@ -0,0 +1,19 @@
+daily = int(input())
+expected = float(input())
+
+
+total = 0
+for day in range(1, days + 1):
+    total += daily
+
+    if day % 3 == 0:
+        total += daily * 0.5
+
+    if day % 5 == 0:
+        total -= total * 0.3
+
+if total >= expected:
+    print(f"Ahoy! {total:.2f} plunder gained.")
+else:
+    percentage = total / expected * 100
+    print(f"Collected only {percentage:.2f}% of the plunder.")
