945a588235349f2fcbf170e29b0851b84ab47785
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-18 19:24:22 +0100

    Update mu online Pre exam exercise.

--- [6] Mid Exam Preparation/[1] Mid-Exam Preparation Part 2/01_mu_online.py
+++ [6] Mid Exam Preparation/[1] Mid-Exam Preparation Part 2/01_mu_online.py
@@ -1,18 +1,37 @@
 rooms = input().split("|")
 
+
 MAX_HEALTH = 100
 health = MAX_HEALTH
 bitcoins = 0
+room_num = 0
 
-room_number = 0
 
 for room in rooms:
-    room_number += 1
+    room_num += 1
     command, amount = room.split()
-    # Try with floats if errors
     amount = int(amount)
 
     if command == "potion":
         if health + amount > MAX_HEALTH:
             print(f"You healed for {MAX_HEALTH - health} hp.")
             health = MAX_HEALTH
+        else:
+            print(f"You healed for {amount} hp.")
+            health += amount
+        print(f"Current health: {health} hp.")
+    elif command == "chest":
+        bitcoins += amount
+        print(f"You found {amount} bitcoins.")
+    else:
+        health -= amount
+        if health > 0:
+            print(f"You slayed {command}.")
+        else:
+            print(f"You died! Killed by {command}.")
+            print(f"Best room: {room_num}")
+            exit(0)
+
+print("You've made it!")
+print(f"Bitcoins: {bitcoins}")
+print(f"Health: {health}")
