6d4fcbba8d03a0af600c50a4c16b93f501c3470d
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-02-20 16:09:38 +0100

    Update.

--- [1] Basic Syntax, Conditional Statements and Loops/[0] Basic Syntax, Conditional Statements and Loops  - Lab/00_number_definer.py
+++ [1] Basic Syntax, Conditional Statements and Loops/[0] Basic Syntax, Conditional Statements and Loops  - Lab/00_number_definer.py
@@ -4,9 +4,10 @@ number = float(input())
 
 if number == 0:
     print("zero")
-elif number < 1:
-    print("small positive")
-    if number > 1000000:
+elif number > 0:
+    if number < 1:
+        print("small positive")
+    elif number > 1000000:
         print("large positive")
     else:
         print("positive")
