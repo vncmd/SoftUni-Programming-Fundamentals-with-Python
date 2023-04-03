e829301d73ae80f57585145fb1203c8e562479a5
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-03 18:51:43 +0200

    Update

--- [1] Basic Syntax, Conditional Statements and Loops/[1] Basic Syntax, Conditional Statements and Loops  - Exercise/09_mutate_strings.py
+++ [1] Basic Syntax, Conditional Statements and Loops/[1] Basic Syntax, Conditional Statements and Loops  - Exercise/09_mutate_strings.py
@@ -1,10 +1,11 @@
-first_str = input()
-second_str = input()
+first_string = input()
+second_string = input()
 
-last_printed = first_str
-for chr_i in range(len(first_str)):
-    left_part = second_str[:chr_i + 1]
-    right_part = first_str[chr_i + 1:]
+last_printed = first_string
+for chr_i in range(len(first_string)):
+
+    left_part = second_string[:chr_i]
+    right_part = first_string[chr_i:]
     current_str = left_part + right_part
     if current_str == last_printed:
         continue
