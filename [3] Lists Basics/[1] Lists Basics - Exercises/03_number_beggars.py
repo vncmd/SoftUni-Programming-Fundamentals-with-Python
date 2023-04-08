c1686881f1fbcd3b07b7a14bd32786070b3a8020
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-08 19:02:57 +0200

    Added different solution.

--- [3] Lists Basics/[1] Lists Basics - Exercises/03_number_beggars.py
+++ [3] Lists Basics/[1] Lists Basics - Exercises/03_number_beggars.py
@@ -9,3 +9,24 @@ for i in range(len(numbers)):
     beggars[beggars_idx] += num
 
 print(beggars)
+
+# Different solution:
+
+# money = input().split(", ")
+# money_num = []
+#
+# for el in money:
+#     money_num.append(int(el))
+#
+# beggars = int(input())
+# total_sum = []
+# start_idx = 0
+#
+# while start_idx < beggars:
+#     current_beggar_cut = 0
+#     for current_idx in range(start_idx, len(money_num), beggars):
+#         current_beggar_cut += money_num[current_idx]
+#     total_sum.append(current_beggar_cut)
+#     start_idx += 1
+#
+# print(total_sum)
