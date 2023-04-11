609f7275bd21d074923dbeb533bf01c7855c92f1
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-11 07:56:54 +0200

    Added different solution.

--- [5] Lists Advanced/[1] Lists Advanced - Exercises/04_office_chairs.py
+++ [5] Lists Advanced/[1] Lists Advanced - Exercises/04_office_chairs.py
@@ -16,3 +16,29 @@ for n_room in range(1, rooms + 1):
 
 if flag:
     print(f"Game On, {free_chairs} free chairs left")
+
+# Different solution:
+
+
+# def rooms_check(numbers_of_rooms):
+#     total_free_chairs = 0
+#     total_needed_chairs = 0
+#
+#     for room in range(1, numbers_of_rooms + 1):
+#         free_chairs, visitors = input().split()
+#         diff = len(free_chairs) - int(visitors)
+#
+#         if diff >= 0:
+#             total_free_chairs += diff
+#         else:
+#             total_needed_chairs += abs(diff)
+#             print(f"{abs(diff)} more chairs needed in room {room}")
+#
+#     return total_free_chairs, total_needed_chairs
+#
+#
+# number_of_rooms = int(input())
+# free_chairs, needed_chairs = rooms_check(number_of_rooms)
+#
+# if free_chairs >= needed_chairs:
+#     print(f"Game On, {free_chairs} free chairs left")
