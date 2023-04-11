2bf4148a7144e8d241ff6cabd42d5fa8c4561e88
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-11 07:28:16 +0200

    Added different solution.

--- [5] Lists Advanced/[1] Lists Advanced - Exercises/01_next_version.py
+++ [5] Lists Advanced/[1] Lists Advanced - Exercises/01_next_version.py
@@ -11,3 +11,17 @@ if patch == 10:
         major += 1
 
 print(f"{major}.{minor}.{patch}")
+
+# Different solution:
+
+# version = [int(num) for num in input().split(".")]
+# version[-1] += 1
+#
+# for index in range(len(version) - 1, -1, -1):
+#
+#     if version[index] > 9:
+#         version[index] = 0
+#         if index - 1 >= 0:
+#             version[index - 1] += 1
+#
+# print('.'.join(str(number) for number in version))
