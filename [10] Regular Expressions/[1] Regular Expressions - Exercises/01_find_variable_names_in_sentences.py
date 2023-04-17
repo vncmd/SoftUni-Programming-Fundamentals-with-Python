2d07cb3a1c315a420bace01ccce6efded045dcd2
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-17 19:30:07 +0200

    Added different solution.

--- [10] Regular Expressions/[1] Regular Expressions - Exercises/01_find_variable_names_in_sentences.py
+++ [10] Regular Expressions/[1] Regular Expressions - Exercises/01_find_variable_names_in_sentences.py
@@ -7,3 +7,18 @@ pattern = r'\b_([A-Za-z0-9]+)\b'
 found_names = re.findall(pattern, input_text)
 
 print(','.join(found_names))
+
+
+# Different solution:
+#
+# import re
+#
+# input_string = input()
+#
+# pattern = re.compile(r"\b_(?P<word>[A-Za-z0-9]+\b)")
+# result = re.finditer(pattern, input_string)
+# match_list = []
+# for show in result:
+#     match_list.append(show["word"])
+#
+# print(','.join(match_list))
