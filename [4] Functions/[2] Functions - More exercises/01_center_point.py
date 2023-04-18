57d801280444fd01f5a3a49ed35e6caa612c07d7
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-18 06:12:27 +0200

    Update

--- [4] Functions/[2] Functions - More exercises/01_center_point.py
+++ [4] Functions/[2] Functions - More exercises/01_center_point.py
@@ -3,18 +3,18 @@ from math import floor
 
 def check_center_point(x, y):
     if x <= y:
-        return f"({first_point_x}, {second_point_x})"
+        return f"({x1}, {x2})"
     elif y <= x:
-        return f"({first_point_y}, {second_point_y})"
+        return f"({y_1}, {y_2})"
 
 
-first_point_x = floor(float(input()))
-second_point_x = floor(float(input()))
+x1 = floor(float(input()))
+x2 = floor(float(input()))
 
-first_point_y = floor(float(input()))
-second_point_y = floor(float(input()))
+y_1 = floor(float(input()))
+y_2 = floor(float(input()))
 
-sum_axis_x = floor(abs(first_point_x) + abs(second_point_x))
-sum_axis_y = floor(abs(first_point_y) + abs(second_point_y))
+sum_axis_x = floor(abs(x1) + abs(x2))
+sum_axis_y = floor(abs(y_1) + abs(y_2))
 
 print(check_center_point(sum_axis_x, sum_axis_y))
