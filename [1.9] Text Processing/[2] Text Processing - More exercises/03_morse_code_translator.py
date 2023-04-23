dc7cf7690ab37370da5d91b215c94ee4391121b3
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-23 20:39:57 +0200

    Output program translates messages from Morse code to English.

---
+++ [1.9] Text Processing/[2] Text Processing - More exercises/03_morse_code_translator.py
@@ -0,0 +1,19 @@
+morse_code = {
+    ".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E",
+    "..-.": "F", "--.": "G", "....": "H", "..": "I", ".---": "J",
+    "-.-": "K", ".-..": "L", "--": "M", "-.": "N", "---": "O",
+    ".--.": "P", "--.-": "Q", ".-.": "R", "...": "S", "-": "T",
+    "..-": "U", "...-": "V", ".--": "W", "-..-": "X", "-.--": "Y", "--..": "Z"
+}
+
+input_code = input().split()
+result = ''
+
+for code in input_code:
+
+    if code in morse_code:
+        result += morse_code[code]
+    elif code == "|":
+        result += ' '
+
+print(result)
