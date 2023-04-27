88065bd9c072339849decfad99e9a715678ae066
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-27 17:11:22 +0200

    Added different solution.

--- [1.8] Dictionaries/[1] Dictionaries - Exercises/03_phonebook.py
+++ [1.8] Dictionaries/[1] Dictionaries - Exercises/03_phonebook.py
@@ -11,3 +11,21 @@ for check in range(int(entry)):
         print(f"{searched_name} -> {phonebook[searched_name]}")
     else:
         print(f"Contact {searched_name} does not exist.")
+
+# Different solution:
+
+# phone_book = {}
+# phone_number = input()
+#
+# while not phone_number.isdigit():
+#     name, number = phone_number.split("-")
+#     phone_book[name] = phone_book.get(name, number)
+#     phone_number = input()
+#
+# for _ in range(int(phone_number)):
+#     name_check = input()
+#
+#     if name_check in phone_book:
+#         print(f"{name_check} -> {phone_book[name_check]}")
+#     else:
+#         print(f"Contact {name_check} does not exist.")
