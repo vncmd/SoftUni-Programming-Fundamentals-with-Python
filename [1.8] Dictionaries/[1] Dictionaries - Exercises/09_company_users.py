218f65755cc21793589c08fc78f499c39ad32238
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-04-28 18:56:39 +0200

    Added different solution

--- [1.8] Dictionaries/[1] Dictionaries - Exercises/09_company_users.py
+++ [1.8] Dictionaries/[1] Dictionaries - Exercises/09_company_users.py
@@ -15,3 +15,32 @@ for current_company in company_data.items():
     employees = current_company[1]
     for employee in range(len(employees)):
         print(f"-- {employees[employee]}")
+
+
+# Different solution:
+
+# def employee_id_search(id):
+#     for value in company_db[id].values():
+#         if id == value:
+#             return True
+#     return False
+#
+#
+# command = input()
+# company_db = {}
+#
+# while command != "End":
+#     company_name, employee_id = command.split(" -> ")
+#
+#     company_db[company_name] = company_db.get(company_name, {})
+#
+#     if not employee_id_search(company_name):
+#         company_db[company_name][employee_id] = employee_id
+#
+#     command = input()
+#
+# for employee in company_db:
+#     print(employee)
+#
+#     for key, value in company_db[employee].items():
+#         print(f"-- {value}")
