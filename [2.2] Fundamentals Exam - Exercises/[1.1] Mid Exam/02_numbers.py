8bc35f3d3fecef6e6f783d14f1611a3b2364e7d4
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-01 20:23:53 +0200

    Added different solution.

--- [2.2] Fundamentals Exam - Exercises/[1.1] Mid Exam/02_numbers.py
+++ [2.2] Fundamentals Exam - Exercises/[1.1] Mid Exam/02_numbers.py
@@ -15,3 +15,28 @@ for idx, num in enumerate(main_list):
         print("No")
 
         break
+
+
+# Different solution:
+
+# nums = [int(num) for num in input().split()]
+
+# sorted_nums = list(sorted(nums, reverse=True))
+# avg_num = sum(sorted_nums) / len(sorted_nums)
+# greater_value = []
+#
+# for num in range(len(sorted_nums)):
+#     current_num = int(sorted_nums[num])
+#
+#     if current_num > avg_num:
+#         greater_value.append(current_num)
+#
+# for num in range(len(greater_value)):
+#     if len(greater_value) > 5:
+#         greater_value.pop(-1)
+#
+# if not greater_value:
+#     print("No")
+#
+# else:
+#     print(*greater_value)
