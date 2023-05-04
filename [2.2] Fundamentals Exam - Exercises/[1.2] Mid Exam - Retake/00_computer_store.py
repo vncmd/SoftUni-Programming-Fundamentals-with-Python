9d59cad0cbf12e41b43c29015aa0ef79937ad734
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-04 13:07:32 +0200

    Update.

--- [2.2] Fundamentals Exam - Exercises/[1.2] Mid Exam - Retake/00_computer_store.py
+++ [2.2] Fundamentals Exam - Exercises/[1.2] Mid Exam - Retake/00_computer_store.py
@@ -37,3 +37,48 @@ if end_price == 0:
 else:
     print(f"Congratulations you've just bought a new computer!\nPrice without taxes: {total_price_without_taxes:.2f}$\n"
           f"Taxes: {total_amount_of_taxes:.2f}$\n-----------\nTotal price: {end_price:.2f}$")
+
+
+# Different solution:
+
+
+def buy_pc(off):
+    taxes = total * 0.20
+
+    global buying_parts
+
+    if off:
+        final_price = (total + taxes) - (total + taxes) * 0.10
+    else:
+        final_price = total + taxes
+
+    buying_parts = False
+    if total == 0:
+        print("Invalid order!")
+
+    else:
+        print("Congratulations you've just bought a new computer!")
+        print(f"Price without taxes: {total:.2f}$")
+        print(f"Taxes: {taxes:.2f}$")
+        print("-----------")
+        print(f"Total price: {final_price:.2f}$")
+
+
+command = 0
+total = 0
+buying_parts = True
+
+while buying_parts:
+    command = float(command)
+
+    if command >= 0:
+        total += command
+    else:
+        print("Invalid price!")
+
+    command = input()
+
+    if command == "special":
+        buy_pc(True)
+    elif command == "regular":
+        buy_pc(False)
