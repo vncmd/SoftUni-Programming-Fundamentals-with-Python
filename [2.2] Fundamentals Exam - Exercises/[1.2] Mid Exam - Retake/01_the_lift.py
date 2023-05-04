2aa163374cc422924d04101fb869b4c86debc026
Vladislav Naydenov Dimov <99009455+vncmd@users.noreply.github.com>
2023-05-04 13:11:20 +0200

    Added different solution.

--- [2.2] Fundamentals Exam - Exercises/[1.2] Mid Exam - Retake/01_the_lift.py
+++ [2.2] Fundamentals Exam - Exercises/[1.2] Mid Exam - Retake/01_the_lift.py
@@ -12,3 +12,33 @@ elif any([wagon < 4 for wagon in current_wagons]):
     print("The lift has empty spots!")
 
 print(*current_wagons)
+
+# Different solution:
+
+people = int(input())
+max_people = 4
+current = [int(num) for num in input().split()]
+
+for el in range(len(current)):
+    if people >= 4:
+        if current[el] == 0:
+            current[el] += 4
+            people -= 4
+        else:
+            c_num = max_people - int(current[el])
+            current[el] += c_num
+            people -= c_num
+    else:
+        current[el] += people
+        people = 0
+
+check_nums = [int(num) for num in current if num != 4]
+
+if people == 0 and check_nums:
+    print("The lift has empty spots!")
+    print(*current, sep=" ")
+if people != 0 and not check_nums:
+    print(f"There isn't enough space! {people} people in a queue!")
+    print(*current, sep=" ")
+if people == 0 and not check_nums:
+    print(*current, sep=" ")
